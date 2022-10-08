60e47d156b1e63237d684440373d332cb7681657
Ofir Peer <112880744+OfirPeer07@users.noreply.github.com>
2022-10-08 18:04:15 +0300

    Update adder.py

--- Calc2Parameters/adder.py
+++ Calc2Parameters/adder.py
@@ -3,9 +3,9 @@ After you have created "lambda_function.py" file, we need to add another file.
 # 1. Create new file.
 # 2. Call the file - "adder.py".
 # 3. Copy the code below into your new file.
-# 4. Make sure you added the line *** from adder import add *** in lambda_function.py file.
+# 4. Make sure you added the line // from adder import add // in lambda_function.py file.
 # 5. Save and Deploy your changes.
 '''
-
+ 
 def add(num1,num2):
     return num1+num2
